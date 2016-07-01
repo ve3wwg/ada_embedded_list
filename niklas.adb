a4347ac1ae445dc42b8143a954cae82751a92f33
Warren Gay <ve3wwg@gmail.com>
2016-07-01 12:21:10 -0400

    rm Ada.Text_IO use

--- niklas.adb
+++ niklas.adb
@@ -1,35 +1,24 @@
-with Ada.Text_IO;
 with Ada.Integer_Text_IO;
 
 package body Niklas is
 
    procedure Insert_At_Head(List: access List_T; Node: Emb_Node_Ref_T) is
-      use Ada.Text_IO;
    begin
 
-      Put_Line("Ins_Start");
       Node.Next := List.Next;
-
-      Put_Line("Ins_A");
       Node.Prev := Emb_Node_Ref_T(List);  -- Exception raised here
       -- raised PROGRAM_ERROR : niklas.adb:14 accessibility check failed
 
-      Put_Line("Ins_B");
       if Node.Next /= Null then
-         Put_Line("Ins_C");
          Node.Next.Prev := Node;
       end if;
 
-      Put_Line("Ins_D");
       List.Next := Node;
-
-      Put_Line("Ins_E");
       List.Prev := Null;
-      Put_Line("Ins_Done");
 
    end Insert_At_Head;
 
-   procedure Delete(Node: in Emb_Node_Ref_T) is
+   procedure Delete(Node: Emb_Node_Ref_T) is
    begin
       if Node.Prev /= Null then
          Node.Prev.Next := Node.Next;
