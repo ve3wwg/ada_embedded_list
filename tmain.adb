ba3b33a7aea2d0703c6244ca47f2bf2c5d74043a
Warren Gay <ve3wwg@gmail.com>
2016-07-01 12:30:49 -0400

    Fixed problem with exception

--- tmain.adb
+++ tmain.adb
@@ -5,10 +5,11 @@ procedure TMain is
    use Niklas;
    use Ada.Text_IO;
 
-   List1:   aliased List_T;
-   One:     access Integer_Object_T := new Integer_Object_T;
-   Two:     access Integer_Object_T := new Integer_Object_T;
-   Three:   access Integer_Object_T := new Integer_Object_T;
+   -- List1:   aliased List_T;  -- raised PROGRAM_ERROR : niklas.adb:14 accessibility check failed
+   List1:      aliased access List_T := new List_T;
+   One:        access Integer_Object_T := new Integer_Object_T;
+   Two:        access Integer_Object_T := new Integer_Object_T;
+   Three:      access Integer_Object_T := new Integer_Object_T;
 begin
 
    One.Value := 1;
@@ -16,13 +17,13 @@ begin
    Three.Value := 3;
    
    Put_Line("Ins1..");
-   Insert_At_Head(List1'Access,Two.Node1'Access);
+   Insert_At_Head(List1,Two.Node1'Access);
    Put_Line("Ins2..");
-   Insert_At_Head(List1'Access,One.Node1'Access);
+   Insert_At_Head(List1,One.Node1'Access);
    Put_Line("Ins3..");
-   Insert_At_Head(List1'Access,Three.Node1'Access);
+   Insert_At_Head(List1,Three.Node1'Access);
 
    Put_Line("Traversal..");
-   Print_Integer_Values(List1);
+   Print_Integer_Values(List1.all);
 
 end TMain;
